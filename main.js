91bc5270ae7130e6af02b42fbe1cc39e8f823fcd
Divyanshi Rastogi <43536638+Divya-5@users.noreply.github.com>
2022-01-04 00:10:41 +0530

    added conversions

---
+++ main.js
@@ -0,0 +1,17 @@
+document.getElementById("kgsInput").addEventListener("input", e => {
+    let kgs = e.target.value;
+    document.getElementById("lbsOutput").innerHTML = kgs * 2.2046;
+})
+
+document.getElementById("feetInput").addEventListener("input", e => {
+    let feet = e.target.value;
+    document.getElementById("cmsOutput").innerHTML = feet * 30.48;
+})
+document.getElementById("celciusInput").addEventListener("input", e => {
+    let celcius = e.target.value;
+    document.getElementById("fahrenOutput").innerHTML = (celcius * 1.8) + 32;
+  })
+  document.getElementById("fahrenInput").addEventListener("input", e => {
+    let farenheit = e.target.value;
+    document.getElementById("celciusOutput").innerHTML =  5/9 * (farenheit - 32) ;
+  })
